62b2937814d0a210b884274474b5e8affc228a72
Wenbiao Li <lixuyao.tony@gmail.com>
2017-11-08 16:55:42 -0500

    delete redundancy

--- config/connnection.js
+++ config/connnection.js
@@ -1,9 +1,17 @@
+var mysql = require('mysql');
+var connection;
 
-
-module.exports  = {
-	socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-	port: 3306,
-	user: 'root',
-	password: 'root',
-	database: 'my_schema'
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+		port: 3306,
+		user: 'root',
+		password: 'root',
+		database: 'my_schema'
+	});
 };
+
+connection.connect();
+module.exports  = connection;
